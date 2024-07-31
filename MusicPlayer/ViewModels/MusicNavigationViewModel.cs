b1631795ce8a6c8c5577fa7eafca46a6c642af2c
schpreter <pe.simon17@gmail.com>
2024-07-31 22:25:15 +0200

    Backup attempt #3

---
+++ MusicPlayer/ViewModels/MusicNavigationViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.ViewModels
+{
+    public partial class MusicNavigationViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        public bool isPaused = true;
+        public void ShuffleClicked()
+        {
+            throw new NotImplementedException();
+        }
+        public void SkipBackClicked()
+        {
+            throw new NotImplementedException();
+        }
+        public void SkipForwardClicked()
+        {
+            throw new NotImplementedException();
+        }
+        public void PausePlayClicked()
+        {
+            throw new NotImplementedException();
+        }
+        public void RepeatClicked()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
